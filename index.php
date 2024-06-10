ac6d65bb5b11ff1c59a8083dd211cccbf478ba88
yFifio <lucasdefiori@gmail.com>
2024-06-10 19:57:14 -0300

    Projeto Modulo 2 form editon v1.00

--- index.php
+++ index.php
@@ -94,6 +94,23 @@
         <img src="./img/icones/github.webp" alt="GitHub" width="80px" height="80px">
     </a>
 
+    <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">Nunca compartilharemos seu e-mail com mais ninguém.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Senha</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Lembre de mim</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Login</button>
+  </form>
+
 </footer>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
